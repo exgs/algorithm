e7809b140c31c16a013415e829b46c46fe5607d2
yunslee <yunslee@student.42seoul.kr>
2021-06-17 15:43:43 +0900

    prgrammers 62048
    수학적 귀납법으로 작은 직사각형에 대한 규칙을 찾아냈다고 생각했는데, 예외가 존재했음
    어쩔 수 없이 slope를 쓰고 계산을 해야하는 문제
    그렇게 수정을 거친뒤에도 통과하지 못하는 것을 보고, 통과가 되는 코드와 비교를 해보았을 떄,
    실수계산에서의 오차에 대한 해결을 하지않은 것을 해답으로 내놓은 것으로 생각됨
    예를들어 5 245 의 답은 980이지, 테스트를 통과에서 해답으로 내놓는 976이 아니라고 생각됨

--- programmers/62048.cpp
+++ programmers/62048.cpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   62048.cpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yunslee <yunslee@student.42seoul.kr>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/07 10:11:06 by yunslee           #+#    #+#             */
-/*   Updated: 2021/06/07 10:11:39 by yunslee          ###   ########seoul.kr  */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <math.h>
 #include <iostream>
 using namespace std;
@@ -32,26 +20,33 @@ int greatest_common_divisor(int a, int b)
 
 long long solution(int w,int h) {
     long long answer = 1;
-	int temp = greatest_common_divisor(w, h);
-	// cout << w << " " << h <<" GCD: " << temp << endl;
-	long long w_div = w / temp;
-	long long h_div = h / temp;
-	int blank_box;
+	int gcd = greatest_common_divisor(w, h);
+	// cout << w << " " << h <<" GCD: " << gcd << endl;
+	long long w_div = w / gcd; // 8byte int
+	long long h_div = h / gcd; // 8byte int
+	int blank_box = 0;
 	if (w_div == h_div)
 	{
 		blank_box = w_div;
 	}
-	else if (w_div > h_div)
-	{
-		blank_box = ceil(double(w_div)/h_div) * h_div;
-	}
 	else
 	{
-		blank_box = ceil(double(h_div)/w_div) * w_div;
+		if (w_div > h_div) // 항상 height가 width보다 크도록 함.
+			swap(w_div, h_div);
+		double slope = ((double)h_div / (double)w_div); // > 1
+		
+		double before = 0;
+		for (int i = 0; i < w_div; i++)
+		{
+			double cur = 0;
+			cur = before + slope;
+			blank_box += (ceil(cur) - floor(before));
+			before = cur;
+		}
 	}
-	unsigned long long temp2;
-	temp2 = (long long)w * h;
-	answer = temp2 - blank_box * temp;
+	long long temp2;
+	temp2 = (long long)w * (long long)h;
+	answer = temp2 - blank_box * gcd;
 	return answer;
 }
 
