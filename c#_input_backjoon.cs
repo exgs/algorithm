0622cca7d7e1b9293a1699d20a01072acc921480
yunslee <yunslee@student.42seoul.kr>
2021-06-23 13:00:22 +0900

    c# input 백준

---
+++ c#_input_backjoon.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace PosisionalPattern
+{
+	class MainApp
+	{
+		static void Main(string[] args)
+		{
+			string[] s = Console.ReadLine().Split();
+			
+			foreach (string temp in s)
+			{
+				Console.WriteLine(temp);
+			}
+			return ;
+		}
+	}
+}
