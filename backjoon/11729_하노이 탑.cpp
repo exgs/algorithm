5ad22cf55bcc12459d2dfe4320aa59c52e570ccb
yunslee <yunslee@student.42seoul.kr>
2021-06-26 09:06:49 +0900

    update 하노이 탑

--- backjoon/11729_하노이 탑.cpp
+++ backjoon/11729_하노이 탑.cpp
@@ -1,40 +1,68 @@
-#include <iostream>
+using System;
+using static System.Console;
+using System.Text;
 
-static int num = 0;
-static void hanoi(int src, int stop, int dst, int N)
+namespace test
 {
-	if (N == 1)
+	class MainApp
 	{
-		printf("%d %d\n", src, dst);
-		return ;
-	}
-	
-	hanoi(src, dst, stop, N - 1);
-	hanoi(src, stop, dst, 1);
-	hanoi(stop, src, dst, N - 1);
-	return ;
-}
+		static void hanoi(int src, int stop, int dst, int N, StringBuilder string_builder)
+		{
+			if (N == 1)
+			{
+				string_builder.AppendFormat(string.Format("{0} {1}\n", src, dst));
+				return ;
+			}
+			
+			hanoi(src, dst, stop, N - 1, string_builder);
+			hanoi(src, stop, dst, 1, string_builder);
+			hanoi(stop, src, dst, N - 1, string_builder);
+			return ;
+		}
 
-static void hanoi_num(int src, int stop, int dst, int N)
-{
-	if (N == 1)
-	{
-		num++;
-		return ;
-	}
-	
-	hanoi_num(src, dst, stop, N - 1);
-	hanoi_num(src, stop, dst, 1);
-	hanoi_num(stop, src, dst, N - 1);
-	return ;
-}
-int main()
-{
-	int N;
-	scanf("%d", &N);
+		static void hanoi(int src, int stop, int dst, int N)
+		{
+			if (N == 1)
+			{
+				Write("{0} {1}\n", src, dst);
+				return ;
+			}
+			
+			hanoi(src, dst, stop, N - 1);
+			hanoi(src, stop, dst, 1);
+			hanoi(stop, src, dst, N - 1);
+			return ;
+		}
 
-	hanoi_num(1, 2, 3, N);
-	printf("%d\n", num);
-	hanoi(1, 2, 3, N);
-	return (0);
+		// static int num = 0;
+		// static void hanoi_num(int src, int stop, int dst, int N)
+		// {
+		// 	if (N == 1)
+		// 	{
+		// 		num++;
+		// 		return ;
+		// 	}
+			
+		// 	hanoi_num(src, dst, stop, N - 1);
+		// 	hanoi_num(src, stop, dst, 1);
+		// 	hanoi_num(stop, src, dst, N - 1);
+		// 	return ;
+		// }
+		static void Main()
+		{
+			// StringBuilder string_builder = new StringBuilder();
+			int N = int.Parse(ReadLine());
+		
+			// hanoi_num(1, 2, 3, N);
+			Write("{0}\n", Math.Pow(2, N) - 1);
+
+			// STUB 타임아웃!
+			hanoi(1, 2, 3, N);
+
+			// STUB 시간줄이기 최적화
+			hanoi(1, 2, 3, N, string_builder);
+			Write(string_builder);
+			return;
+		}
+	}
 }
