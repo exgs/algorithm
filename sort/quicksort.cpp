2a7c58571863b51ed9df82b143b553e178d6f98c
yunslee <yunslee@student.42seoul.kr>
2021-07-04 22:04:02 +0900

    quicksort

--- sort/quicksort.cpp
+++ sort/quicksort.cpp
@@ -1,58 +1,51 @@
+//https://www.youtube.com/watch?v=cWH49IKDIiI&t=282s
 #include <iostream>
 using namespace std;
 
-void swap(int &a, int &b)
+void print_nums(int array[], size_t count)
 {
-	int temp = a;
-	a = b;
-	b = temp;
-}
-
-void print_nums(int nums[], size_t count)
-{
-	cout << "------------ print nums ------------" << endl;
+	cout << "--------- print_nums ---------" << endl;
 	for (size_t i = 0; i < count; i++)
 	{
-		cout << nums[i] << " ";
-		if (i != 0 && i % 10 == 9)
+		cout << array[i] << " ";
+		if (i != 0 && i % 10 == 0)
 			cout << endl;
 	}
 	cout << endl;
 }
 
-/* 완벽하게 들어맞는지 확신이 안드는 코드*/
-void quicksort(int *array, size_t start, size_t end)
+int partition(int array[], int start, int end)
 {
-	if ((int)end <= (int)start)
-		return ;
-	int pivot_value = array[(start + end) / 2];
-	size_t left = start;
-	size_t right = end;
-	while (left <= right)
+	int pivot_value = array[end];
+	int where_pivot = start - 1;
+	for (size_t i = start; i <= end - 1; i++)
 	{
-		print_nums(array, 7);
-		while (array[left] < pivot_value)
-			left++; // array[left] >= pivot_value
-		while (array[right] > pivot_value)
-			right--; // array[right] <= pivot_value
-		if (left <= right)
+		if (pivot_value >= array[i])
 		{
-			swap(array[left], array[right]);
-			left++; // left index를 포함한 앞쪽 index는 정렬이 된 상태, 따라서 +1
-			right--; // right index를 포함한 뒷쪽 index는 정렬이 된 상태, 따라서 -1
+			where_pivot++;
+			if (where_pivot != i)
+				swap(array[i], array[where_pivot]);
 		}
 	}
-	quicksort(array, start, right);
-	quicksort(array, left, end);
+	swap(array[end], array[where_pivot + 1]);
+	return (where_pivot + 1);
+}
+
+void quicksort(int array[], int start, int end)
+{
+	if (start >= end)
+		return ;
+	int pivot_index = partition(array, start, end);
+	quicksort(array, start, pivot_index - 1);
+	quicksort(array, pivot_index + 1, end);
 }
 
 
 int main()
 {
 	// int lists[24] = {5,3,4,2,1,1,6,4,3,2,6,4,9,5,3,2,6,8,3,5,7,4,3,1};
-	int lists[7] = {3,2,1,9,14,5,10};
-
-	// int lists[3] = {3, 2, 0};
+	int lists[3] = {3, 2, 0};
+	// int lists[3] = {0, 1, 2};
 	// int lists[4] = {3, 2, 1, 0};
 	// int lists[2] = {3, 2};
 	// int lists[5] = {3, 2, 6, 4, 0};
@@ -61,5 +54,5 @@ int main()
 	print_nums(lists, count);
 	quicksort(lists, 0, count - 1);
 	print_nums(lists, count);
-	return (0);
+	return (1);
 }
