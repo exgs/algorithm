017a375fc1d3585b5c99a6aa052b07023c708a32
yunslee <yunslee@student.42seoul.kr>
2021-07-04 21:20:52 +0900

    quicksort_Not Sure

--- sort/quicksort.cpp
+++ sort/quicksort.cpp
@@ -20,6 +20,7 @@ void print_nums(int nums[], size_t count)
 	cout << endl;
 }
 
+/* 완벽하게 들어맞는지 확신이 안드는 코드*/
 void quicksort(int *array, size_t start, size_t end)
 {
 	if ((int)end <= (int)start)
@@ -29,14 +30,16 @@ void quicksort(int *array, size_t start, size_t end)
 	size_t right = end;
 	while (left <= right)
 	{
+		print_nums(array, 7);
 		while (array[left] < pivot_value)
-			left++;
+			left++; // array[left] >= pivot_value
 		while (array[right] > pivot_value)
-			right--;
+			right--; // array[right] <= pivot_value
 		if (left <= right)
 		{
 			swap(array[left], array[right]);
-			left++; right--;
+			left++; // left index를 포함한 앞쪽 index는 정렬이 된 상태, 따라서 +1
+			right--; // right index를 포함한 뒷쪽 index는 정렬이 된 상태, 따라서 -1
 		}
 	}
 	quicksort(array, start, right);
@@ -46,7 +49,9 @@ void quicksort(int *array, size_t start, size_t end)
 
 int main()
 {
-	int lists[24] = {5,3,4,2,1,1,6,4,3,2,6,4,9,5,3,2,6,8,3,5,7,4,3,1};
+	// int lists[24] = {5,3,4,2,1,1,6,4,3,2,6,4,9,5,3,2,6,8,3,5,7,4,3,1};
+	int lists[7] = {3,2,1,9,14,5,10};
+
 	// int lists[3] = {3, 2, 0};
 	// int lists[4] = {3, 2, 1, 0};
 	// int lists[2] = {3, 2};
