7e53d785f528253c9312788d3f6510e64f6b099e
yunslee <yunslee@student.42seoul.kr>
2021-07-05 19:51:23 +0900

    mergesort

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -94,24 +94,82 @@ void shellsort(int *array, size_t count)
 	
 }
 
-int *mergesort(int *array, s_data data)
+void setting_data(s_data *data, int count)
 {
-	return (NULL);
+	data->size = count;
+	data->front_size = count / 2; // 홀수이면, 뒷 데이터보다 하나 적음
+	data->back_size = data->size - data->front_size;
 }
 
-void divid(int *lists, s_data data, int **temp1, int **temp2)
+int *mergesort(int *array, s_data mergedata)
 {
-
+	if (array == NULL)
+		return (NULL);
+	int *temp1 = NULL; int *temp2= NULL; int *temp3 = NULL;
+	divid(array, mergedata, &temp1, &temp2);
+	temp3 = merge(temp1, temp2, mergedata);
+	return temp3;
 }
 
-void setting_data(s_data &data, int count)
+void divid(int *lists, s_data mergedata, int **temp1, int **temp2)
 {
+	if (mergedata.size == 1)
+	{
+		*temp2 = new int[mergedata.back_size];
+		(*temp2)[0] = lists[0];
+		return ;
+	}
+	*temp1 = new int[mergedata.front_size];
+	*temp2 = new int[mergedata.back_size];
+	int i = 0;
+	while (i < mergedata.front_size)
+	{
+		(*temp1)[i] = lists[i];
+		i++;
+	}
+	int j = 0;
+	while (j < mergedata.back_size)
+	{
+		(*temp2)[j] = lists[i + j];
+		j++;
+	}
+	s_data mergedata1;
+	setting_data(&mergedata1, mergedata.front_size);
+	s_data mergedata2;
+	setting_data(&mergedata2, mergedata.back_size);
 
+	int *before_temp1 = *temp1; int *before_temp2 = *temp2;
+	*temp1 = mergesort(before_temp1, mergedata1);
+	*temp2 = mergesort(before_temp2, mergedata2);
+	delete []before_temp1; delete []before_temp2;
 }
 
-int *merge(int *temp1, int *temp2, s_data data)
+
+int *merge(int *temp1, int *temp2, s_data mergedata)
 {
-	return (NULL);
+	int *temp3 = new int[mergedata.size];
+	int i = 0; int j = 0; int z = 0;
+	while (z < mergedata.size)
+	{
+		if (i == mergedata.front_size)
+		{
+			temp3[z++] = temp2[j++];
+		}
+		else if (j == mergedata.back_size)
+		{
+			temp3[z++] = temp1[i++];
+		}
+		else if (temp1[i] <= temp2[j])
+		{
+			temp3[z++] = temp1[i++];
+		}
+		else if (temp1[i] > temp2[j])
+		{
+			temp3[z++] = temp2[j++];
+		}
+	}
+	delete []temp1; delete []temp2;
+	return (temp3);
 }
 
 int partition(int array[], int start, int end)
@@ -131,14 +189,22 @@ int main()
 	// int lists[1] = {100};
 	// int lists[5] = {3, 2, 6, 4, 0};
 	size_t count = sizeof(lists)/sizeof(lists[0]);
-
+	s_data merge_data;
 	print_nums(lists, count);
+
 	// bubblesort(lists, count);
 	// selectsort(lists, count);
 	// insertsort(lists, count);
-	shellsort(lists, count);
+	// shellsort(lists, count);
 
+	/* mergesort는 반환값이 있어서 특별함 */
+	// setting_data(&merge_data, count);
+	// int *temp = mergesort(lists, merge_data);
+	// print_nums(temp, count);
+
+	quicksort(lists, 0, count - 1);
 	print_nums(lists, count);
 
+	system("leaks a.out");
 	return (0);
 }
