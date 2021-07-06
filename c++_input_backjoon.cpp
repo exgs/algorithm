1a557cc133b2e0723cc0fdce28d925199935816d
yunslee <yunslee@student.42seoul.kr>
2021-07-06 14:01:28 +0900

    c++ template 업로드

---
+++ c++_input_backjoon.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	int n; scanf("%d", &n);
+	printf("%d\n", n);
+
+
+	
+	return(0);
+}
