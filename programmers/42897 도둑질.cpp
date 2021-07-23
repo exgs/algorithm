9076f0a4ede2a7a98c1e63e1fadc03d5eee92b8c
yunslee <yunslee@student.42seoul.kr>
2021-07-24 01:29:06 +0900

    42897 도둑질 프로그래머스, not yet...

---
+++ programmers/42897 도둑질.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//  1,000,000개 이하입니다.
+int dp1[1000000];
+int dp2[1000001];
+
+// 첫번째 집 턴다.
+int case1(vector<int> money)
+{
+	dp1[0] = money[0];
+	dp1[1] = money[1];
+	for (size_t i = 2; i < money.size() - 1; i++)
+	{
+		dp1[i] = max(dp1[i - 1], dp1[i - 2] + money[i]);
+		dp1[i - 1] = max(dp1[i - 1], dp1[i - 2]);
+	}
+	return (dp1[money.size() - 2]);
+}
+
+// 첫번째 집 안턴다.
+int case2(vector<int> money)
+{
+	dp2[1] = money[1];
+	dp2[2] = money[2];
+	for (size_t i = 3; i < money.size(); i++)
+	{
+		dp2[i] = max(dp2[i - 1], dp2[i - 2] + money[i]);
+		dp2[i - 1] = max(dp2[i - 1], dp2[i - 2]);
+	}
+	return (dp2[money.size() - 1]);
+}
+
+int solution(vector<int> money)
+{
+	int answer = 0;
+	answer = max(case1(money), case2(money));
+	return answer;
+}
+
+int main()
+{
+	vector<int> vector1 = {1, 2, 3, 1};
+	vector<int> vector2 = {1000, 12, 2, 2, 1000};
+	cout << solution(vector1) << endl;
+	return (0);
+}
+
+/*
+[1, 2, 3, 1]	4
+이 마을에 있는 집은 3개 이상 1,000,000개 이하입니다.
+money 배열의 각 원소는 0 이상 1,000 이하인 정수입니다.
+*/
