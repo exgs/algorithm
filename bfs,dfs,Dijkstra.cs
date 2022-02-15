be0aa577e3d88e8c5b405fd1d764434df04638a5
yunslee <yunslee@student.42seoul.kr>
2022-02-15 14:51:41 +0900

    Dijkstra 추가

--- bfs,dfs,Dijkstra.cs
+++ bfs,dfs,Dijkstra.cs
@@ -15,6 +15,42 @@ namespace HelloWorld
 	{
 		static int length = 0;
 		static bool []havebeenthere;
+		static bool []boolCheck;
+		static int []intCheck;
+		static int matrixSize;
+		
+		static void Dijkstra(int[,] graph, int rootidx)
+		{
+			var que = new Queue<int>();
+			que.Enqueue(rootidx);
+
+			Action print = () =>
+			{
+				for (int i = 0; i < matrixSize; i++)
+				{
+					Console.WriteLine($"목표 노드: {i} => {intCheck[i]}");
+				}
+			};
+			while(que.Count() != 0)
+			{
+				int i = que.Dequeue();
+				WriteLine($"방문한 노드: {i}");
+				boolCheck[i] = true;
+				for (int j = 0; j < matrixSize; j++)
+				{
+					if (graph[i, j] > 0)
+					{
+						intCheck[j] = Math.Min((intCheck[i] + graph[i, j]), intCheck[j]);
+					}
+				}
+				var intCheckList = intCheck.Where((c, i) => boolCheck[i] == false).ToList();
+				if (intCheckList.Count() == 0)
+					break;
+				int temp = intCheck.ToList().IndexOf(intCheckList.Min());
+				que.Enqueue(temp);
+			}
+			print();
+		}
 		static void dfs(int[,] graph, int i)
 		{
 			WriteLine(i);
@@ -78,6 +114,20 @@ namespace HelloWorld
 			havebeenthere = havebeenthere.Select(_ => false).ToArray();
 			WriteLine("-----");
 			bfs(abc, 0);
+			WriteLine("---다익스트라---");
+			matrixSize = 6;
+			var matrix = new int [6, 6]
+			{{0,5,2,0,0,0},
+			 {0,0,0,4,2,0},
+			 {0,8,0,0,5,0},
+			 {0,0,0,0,6,3},
+			 {0,0,0,0,0,1},
+			 {0,0,0,0,0,0},
+			};
+			intCheck = Enumerable.Range(0, matrixSize).Select(_ => int.MaxValue / 2).ToArray();
+			boolCheck = Enumerable.Range(0, matrixSize).Select(_ => false).ToArray();
+			intCheck[0] = 0;
+			Dijkstra(matrix, 0);
 		}
 	}
 }
